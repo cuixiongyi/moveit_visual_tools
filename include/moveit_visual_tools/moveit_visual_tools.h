f46497d549726be44c7bf88444a2b5a234ddc38e
Xiongyi Cui <cuixiongyi2013@gmail.com>
2017-06-09 23:50:09 -0400

    change CMake

--- include/moveit_visual_tools/moveit_visual_tools.h
+++ include/moveit_visual_tools/moveit_visual_tools.h
@@ -63,6 +63,7 @@
 
 namespace moveit_visual_tools
 {
+
 // Default constants
 static const std::string ROBOT_DESCRIPTION = "robot_description";  // this is the default used in ROS
 static const std::string DISPLAY_PLANNED_PATH_TOPIC =
@@ -188,7 +189,7 @@ public:
    */
   void loadTrajectoryPub(const std::string &display_planned_path_topic = DISPLAY_PLANNED_PATH_TOPIC,
                          bool blocking = true);
-  void loadRobotStatePub(const std::string &robot_state_topic = "", bool blocking = true);
+  void loadRobotStatePub(const std::string &robot_state_topic = DISPLAY_ROBOT_STATE_TOPIC, bool blocking = true);
 
   /**
    * \brief Allow a pre-configured planning scene monitor to be set for publishing collision objects, etc
